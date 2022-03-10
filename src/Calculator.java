315f1c1c660928d07a61d79cbaad26113234864a
tihan0 <alexttmsc55@gmail.com>
2022-03-10 21:43:10 +0300

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,5 @@
+public class Calculator {
+    public static void main(String[] args){}
+    Calculator calc = new Calculator(3,4);
+    System.out.println(calc.Sum());
+}
---
+++ src/calc.java
@@ -0,0 +1,13 @@
+public class calc {
+        double a;
+        double b;
+
+        public calc(double a, double b){
+            this.a=a;
+            this.b=b;
+        }
+
+        public int Sum(){
+            return a+b;
+        }
+}
